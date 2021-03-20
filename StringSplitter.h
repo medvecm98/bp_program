e331f6f6f2aa78c5040de413e9c33f461fd9add9
Michal Medvecky <miskomedvecky@gmail.com>
2021-03-20 20:37:59 +0100

    Minor changes to string helpers.

--- StringSplitter.h
+++ StringSplitter.h
@@ -1,32 +1,48 @@
-//
-// Created by michal on 17. 3. 2021.
-//
-
 #include <string>
 #include <vector>
 #include <fstream>
 #include <sstream>
 #include <cctype>
+#include "GlobalUsing.h"
 
 #ifndef PROGRAM_STRINGSPLITTER_H
 #define PROGRAM_STRINGSPLITTER_H
 
 class StringHelpers {
 public:
-	static std::vector<std::string> Split(const std::string& f, char delimiter, bool ignore_empty = false) {
+	/** \brief Splits string with given delimiter.
+	 *
+	 * May filter out empty entries in resulting vector.
+	 * @param f String to split
+	 * @param delimiter Delimiter used to do split
+	 * @param ignore_empty If empty entries should end up in resulting vector
+	 * @return Vector of split string.
+	 */
+	static std::vector<my_string> Split(const my_string& f, char delimiter,
+									 int64_t where_begin = 0, bool ignore_empty = false) {
 		auto ss = std::stringstream(f);
-		return Split(ss, delimiter, ignore_empty);
+		return Split(ss, delimiter, where_begin, ignore_empty);
 	}
 
-	static std::vector<std::string> Split(std::istream& f, char delimiter, bool ignore_empty = false) {
-		auto rv = std::vector<std::string>();
+	/** \brief Splits stream with given delimiter.
+	 *
+	 * May filter out empty entries in resulting vector.
+	 * @param f `istream` to read from
+	 * @param delimiter Delimiter used to do split
+	 * @param ignore_empty If empty entries should end up in resulting vector
+	 * @return Vector of split stream.
+	 */
+	static std::vector<my_string> Split(std::istream& f, char delimiter,
+									 int64_t where_begin = 0, bool ignore_empty = false) {
+		//TODO: utilize `where_begin`
+		auto rv = std::vector<my_string>();
 
-		std::string buffer = "";
+		my_string buffer = "";
 		char cbuf = '~';
 
 		while (f.get(cbuf)) {
 			if (cbuf == delimiter) {
-				if (ignore_empty && buffer.length() == 0) {}
+				if (ignore_empty && buffer.empty()) {}
 				else {
 					rv.push_back(buffer);
 					buffer.clear();
@@ -44,8 +60,15 @@ public:
 		return rv;
 	}
 
-	static std::string RemoveWhitespace(const std::string& source) {
-		std::string buffer;
+	/** \brief Removes any whitespace.
+	 *
+	 * **Any** whitespace from provided string is removed. Uses `std::isspace()`.
+	 *
+	 * @param source String to remove whitespaces from.
+	 * @return String with no whitespaces.
+	 */
+	static my_string RemoveWhitespace(const my_string& source) {
+		my_string buffer;
 		for (auto&& c : source) {
 			if (!std::isspace(c)) {
 				buffer.push_back(c);
@@ -54,13 +77,23 @@ public:
 		return buffer;
 	}
 
-	static std::string Trim(const std::string& source) {
-		std::string buffer;
+	/** \brief Trims whitespace characters.
+	 *
+	 * Trims whitespace characters (using `std::isspace()`) from beginning and end.
+	 * @param source String to trim.
+	 * @return Trimmed string.
+	 */
+	static my_string Trim(const my_string& source) {
+		my_string buffer;
+
+		if (!std::isspace(*source.begin()) && !std::isspace(*source.rbegin())) {
+			return source;
+		}
 
 		int fit = 0;
 		int rit = source.length() - 1;
 
-		while (fit < rit && source[fit] == ' ') {
+		while (fit < rit && std::isspace(source[fit])) {
 			fit++;
 		}
 
@@ -68,7 +101,7 @@ public:
 			return "";
 		}
 
-		while (rit > 0 && source[rit] == ' ') {
+		while (rit > 0 && std::isspace(source[rit])) {
 			rit--;
 		}
 
