c619ac3e7896c64715eae846c1859a4db9548c50
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-17 21:46:31 +0200

    Changed messages to generate protobuf messages.

--- Message.cpp
+++ Message.cpp
@@ -1,78 +1,26 @@
 #include "Message.h"
 
-template<typename T>
-my_vector SerializeT(T input) {
-	auto a = (uint8_t*) &input;
-	my_vector rv;
-	for (std::size_t i = 0; i < sizeof(T); i++) {
-		rv.push_back(*(a + i));
-	}
-	return std::move(rv);
+np2ps::Message MessageFactoryWrapper::IpAddressFactory(pk_t from, pk_t to) {
+	np2ps::Message msg;
+	msg.set_from(from);
+	msg.set_to(to);
+	msg.set_msg_type(np2ps::IP_ADDRESS);
+	msg.set_msg_ctx(np2ps::REQUEST);
+	return std::move(msg);
 }
 
-template<typename T>
-void DeserializeT(T& output, my_vector& input) {
-	auto a = (uint8_t*) &output;
-	for (std::size_t i = sizeof(T); i > 0; i--) {
-		*(a + i - 1) = input.back();
-		input.pop_back();
-	}
+np2ps::Message MessageFactoryWrapper::MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin) {
+	np2ps::Message msg;
+	msg.set_from(from);
+	msg.set_to(to);
+	msg.set_msg_type(np2ps::UPDATE_MARGIN);
+	msg.set_msg_ctx(np2ps::REQUEST);
+
+	msg.mutable_update_margin()->set_m_action(np2ps::ADD);
+	msg.mutable_update_margin()->set_article_pk(article_hash);
+
+	msg.mutable_update_margin()->mutable_margin()->set_type(margin.type);
+	msg.mutable_update_margin()->mutable_margin()->set_content(margin.content);
+	return std::move(msg);
 }
 
-/**
- * \brief Serializes and moves contents of field into vector.
- *
- * @tparam T Source field type.
- * @param rv Vector to move into.
- * @param t Field to serialize.
- * @return Number of bytes of serialized vector.
- */
-template <typename T>
-std::size_t move_vector(my_vector& rv, T&& t) {
-	auto temp = SerializeT(t);
-	rv.insert(rv.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
-	return rv.size();
-}
-
-my_vector Message::Serialize() {
-	my_vector rv;
-	move_vector(rv, to_);
-	move_vector(rv, from_);
-	move_vector(rv, msg_context_);
-	move_vector(rv, msg_type_);
-	return std::move(rv);
-}
-
-bool Message::Deserialize(my_vector s) {
-	DeserializeT(msg_type_, s);
-	DeserializeT(msg_context_, s);
-	DeserializeT(from_, s);
-	DeserializeT(to_, s);
-	return true;
-}
-
-my_vector ArticleHeaderMessage::Serialize() {
-	std::size_t serialized_size = 0;
-	my_vector rv = Message::Serialize();
-	move_vector(rv, article_hash_);
-	serialized_size = move_vector(rv, level_);
-	if (msg_context_ == MessageContext::response) {
-		serialized_size = move_vector(rv, article_);
-	}
-
-	move_vector(rv, serialized_size);
-	return std::move(rv);
-}
-
-bool ArticleHeaderMessage::Deserialize(my_vector s) {
-
-	std::size_t serialized_size = 0;
-	DeserializeT(serialized_size, s); //gets message length
-
-	if (msg_context_ == MessageContext::response)
-		DeserializeT(article_, s);
-	DeserializeT(level_, s);
-	DeserializeT(article_hash_, s);
-	Message::Deserialize(s);
-	return true;
-}
--- Message.h
+++ Message.h
@@ -4,102 +4,12 @@
 #include "GlobalUsing.h"
 #include "Article.h"
 #include "IpWrapper.h"
+#include "protobuf_source/messages.pb.h"
 
-struct MessageType {
-	enum E {
-		empty = 0,
-		articleAll = 10,
-		articleHeader = 20,
-		userIsMember = 30,
-		articleDownload = 40,
-		articleRemoval = 50,
-		handshake = 60,
-		updateMarginAdd = 70,
-		updateMarginRemove = 80,
-		updateArticle = 90,
-		ipAddress = 100
-	};
-};
-
-struct MessageContext {
-	enum E {
-		empty = 0,
-		request = 10,
-		response = 20
-	};
-};
-
-using my_vector = std::vector<uint8_t>;
-
-class Message {
+class MessageFactoryWrapper {
 public:
-	virtual ~Message() noexcept {}
-	virtual my_vector Serialize();
-	virtual bool Deserialize(my_vector);
-protected:
-	Message(pk_t from, pk_t to) {
-		from_ = from;
-		to_ = to;
-		msg_type_ = MessageType::empty;
-		msg_context_ = MessageContext::empty;
-	}
-	pk_t from_, to_;
-	MessageType::E msg_type_;
-	MessageContext::E msg_context_;
-};
-
-class ArticleHeaderMessage : public Message {
-public:
-	explicit ArticleHeaderMessage() : Message(0,0) {
-		article_hash_ = 0;
-		level_ = 0;
-	}
-
-	ArticleHeaderMessage(hash_t req, level_t lvl, pk_t from, pk_t to) : Message(from, to) {
-		msg_context_ = MessageContext::request;
-		msg_type_ = MessageType::articleHeader;
-
-		from_ = from;
-		to_ = to;
-
-		article_hash_ = req;
-		level_ = lvl;
-	}
-
-	ArticleHeaderMessage(const Article& article, level_t lvl, pk_t from, pk_t to) : Message(from, to) {
-		msg_context_ = MessageContext::response;
-		msg_type_ = MessageType::articleHeader;
-
-		from_ = from;
-		to_ = to;
-
-		article_hash_ = article.get_main_hash();
-		article_ = article;
-		level_ = lvl;
-	}
-
-	my_vector Serialize() override;
-	bool Deserialize(my_vector) override;
-
-protected:
-	hash_t article_hash_;
-	level_t level_;
-	Article article_;
-};
-
-class IpAddressMessage : public Message {
-public:
-	IpAddressMessage(pk_t from, pk_t to) : Message(from, to), ip_("") {
-		msg_context_ = MessageContext::request;
-		msg_type_ = MessageType::ipAddress;
-	}
-
-	IpAddressMessage(pk_t from, pk_t to, IpWrapper ip) : Message(from, to), ip_(std::move(ip)) {
-		msg_context_ = MessageContext::response;
-		msg_type_ = MessageType::ipAddress;
-	}
-protected:
-	IpWrapper ip_;
+	static np2ps::Message IpAddressFactory(pk_t from, pk_t to);
+	static np2ps::Message MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin);
 };
 
 
