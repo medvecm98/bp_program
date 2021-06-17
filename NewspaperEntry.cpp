fc5d62a669c06ac42fb9c33a790a0e0f1e09688a
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-17 21:45:25 +0200

    More database methods.

--- NewspaperEntry.cpp
+++ NewspaperEntry.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "NewspaperEntry.h"
+
+NewspaperEntry::NewspaperEntry(pk_t first_key) {
+	_authorities.insert(first_key);
+}
+
+void NewspaperEntry::add_article(hash_t article_hash, Article&& article) {
+	_articles.insert({article_hash, article});
+}
+
+void NewspaperEntry::remove_article(hash_t article_hash) {
+	_articles.erase(article_hash);
+}
+
+std::optional<article_ptr> NewspaperEntry::find_article_header(hash_t article_hash) {
+	auto find_result = _articles.find(article_hash);
+	if (find_result == _articles.end()) {
+		return article_optional ();
+	}
+	return article_optional (std::make_shared<Article>(find_result->second));
+}
+
+database_iterator_t NewspaperEntry::get_const_iterator_database() const {
+	return _articles.cbegin();
+}
+
+database_iterator_t NewspaperEntry::get_const_iterator_database_end() const {
+	return _articles.cend();
+}
--- NewspaperEntry.h
+++ NewspaperEntry.h
@@ -3,13 +3,25 @@
 
 #include "GlobalUsing.h"
 #include "Article.h"
+#include <optional>
+
+using database_iterator_t = article_database_container::const_iterator;
+
 
 /**
  * IDs saved as keys.
  */
 class NewspaperEntry {
+public:
+	NewspaperEntry() = default;
+	explicit NewspaperEntry(pk_t first_key);
+	void add_article(hash_t article_hash, Article&& article);
+	void remove_article(hash_t article_hash);
+	std::optional<article_ptr> find_article_header(hash_t article_hash);
+	database_iterator_t get_const_iterator_database() const;
+	database_iterator_t get_const_iterator_database_end() const;
 private:
-	article_database_container _articles; //downloaded articles by these newspaper
+	article_database_container _articles; //downloaded articles from these newspaper
 	user_container _authorities; //authorities of these newspaper
 };
 
