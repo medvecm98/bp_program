93bd524c83850404cc0cfc3830bcdc0aae1ec469
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-17 21:46:44 +0200

    Moved article stuff away

--- GlobalUsing.h
+++ GlobalUsing.h
@@ -8,23 +8,11 @@
 
 using pk_t = std::size_t;
 
-struct AuthorPeers {
-	pk_t author;
-	std::set<pk_t> peers;
-	explicit AuthorPeers(pk_t a) {
-		author = a;
-	}
-};
-
 using my_string = std::string;
 using hash_t = std::size_t;
 using level_t = std::uint32_t;
 using user_container = std::unordered_set<pk_t>;
 
-/*
- * Category -> Article -> Author, Readers (those, who downloaded those articles)
- */
-using user_multimap_container = std::unordered_map<hash_t, AuthorPeers>;
-using category_multimap_container = std::unordered_multimap<my_string, user_multimap_container>;
+
 
 #endif //PROGRAM_GLOBALUSING_H
