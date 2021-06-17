208da383e6470dc4600b2ed64fffbb2d6146440c
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-17 21:45:38 +0200

    Reworking networking.

--- Networking.cpp
+++ Networking.cpp
@@ -5,11 +5,11 @@ bool Networking::enroll_message_to_be_sent(ProtoMessage&& message) {
     return true;
 }
 
-void Networking::send_message(tcp::socket& tcp_socket) {
+void Networking::send_message(tcp::socket& tcp_socket, msg_queue& send_mq) {
     //serialize message
     std::string serialized_msg;
-    to_send_msg.front()->SerializeToString(&serialized_msg);
-    to_send_msg.pop();
+	send_mq.front()->SerializeToString(&serialized_msg);
+	send_mq.pop();
 
     //append length of message to the message, without the length itself
     std::stringstream length_plus_msg;
@@ -21,16 +21,22 @@ void Networking::send_message(tcp::socket& tcp_socket) {
 }
 
 ProtoMessage Networking::receive_message(tcp::socket& tcp_socket) {
-	//receive
-	std::string recv_msg_len;
+	//receive message length
+	char recv_msg_len[16 + 1];
 	boost::asio::read( tcp_socket, boost::asio::buffer( recv_msg_len, 16));
-	std::size_t msg_len = std::stoll(recv_msg_len, 0, 16);
-	std::string msg;
-	msg.reserve(msg_len);
+	recv_msg_len[16] = '\0';
+	std::size_t msg_len = std::stoll(std::string(recv_msg_len), nullptr, 16);
+
+	//read method requires char array
+	//receive message
+	char msg[msg_len + 1];
 	boost::asio::read( tcp_socket, boost::asio::buffer( msg, msg_len));
+	msg[msg_len] = '\0';
+
+	std::string s_msg(msg);
 
 	//deserialize
 	ProtoMessage m;
-	m.ParseFromString(msg);
+	m.ParseFromString(s_msg);
 	return m;
 }
--- Networking.h
+++ Networking.h
@@ -12,7 +12,13 @@
 
 using boost::asio::ip::tcp;
 
+class PeerSession;
+
 using ProtoMessage = np2ps::Message;
+using unique_ptr_message = std::unique_ptr< ProtoMessage>;
+using msg_session_pair = std::pair< unique_ptr_message, PeerSession>;
+using msg_session_queue = std::queue< msg_session_pair>;
+using msg_queue = std::queue< unique_ptr_message>;
 
 class Networking {
 public:
@@ -20,20 +26,62 @@ public:
 	bool enroll_message_to_be_sent(ProtoMessage&& message);
 
 	IpMap ip_map_;
+	static void send_message(tcp::socket&, msg_queue&);
+	static ProtoMessage receive_message(tcp::socket&);
 private:
-	void send_message(tcp::socket& tcp_socket);
-	ProtoMessage receive_message(tcp::socket& tcp_socket);
 	const std::string port_ = "14128";
 
-	std::queue< std::unique_ptr< ProtoMessage>> to_send_msg;
-	std::queue< std::unique_ptr< ProtoMessage>> received_msg;
+	msg_queue to_send_msg, received_msg;
 	
 	//boost::asio necessities
 	boost::asio::io_context io_ctx;
-	tcp::socket socket_;
-	tcp::resolver resolver_;
-	tcp::acceptor acceptor_;
 };
 
+/**
+ * Handles one session with one specific Peer.
+ */
+class PeerSession {
+public:
+	PeerSession(tcp::socket socket, msg_queue& rmq, msg_queue& smq)
+			: socket_(std::move(socket)), receive_mq_(rmq), send_mq_(smq)
+	{}
+
+	void start() {
+		read_message_from_network();
+	}
+private:
+	void read_message_from_network() {
+		ProtoMessage msg = Networking::receive_message(socket_);
+		receive_mq_.push(std::make_unique< ProtoMessage>(std::move(msg)));
+	}
+	void write_message_into_network() {
+		Networking::send_message(socket_, send_mq_);
+	}
+	msg_queue& receive_mq_;
+	msg_queue& send_mq_;
+	tcp::socket socket_;
+};
+
+class PeerServer {
+public:
+	PeerServer(boost::asio::io_context& io_context, short port,
+			   msg_queue& rmq, msg_queue& smq)
+			: acceptor_(io_context, tcp::endpoint (tcp::v4(), port))
+	{
+		handle_message_accept(rmq, smq);
+	}
+
+	void handle_message_accept(msg_queue& rmq, msg_queue& smq) {
+		acceptor_.async_accept(
+				[this, &rmq, &smq](boost::system::error_code ec, tcp::socket socket) {
+					if (!ec) {
+						std::make_shared< PeerSession>(std::move(socket), rmq, smq)->start();
+					}
+					handle_message_accept(rmq, smq);
+				});
+	}
+private:
+	tcp::acceptor acceptor_;
+};
 
 #endif //PROGRAM_NETWORKING_H
