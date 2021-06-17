220eeb2025f7aaf18161a6cec186e586ecf863ff
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-17 21:46:53 +0200

    Nothing here, for now.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,5 @@
 #include "Networking.h"
 
 int main() {
-	ArticleHeaderMessage m(123, 255, 123, 124);
-	auto s = m.Serialize();
-	ArticleHeaderMessage n(0,0,0,0);
-	n.Deserialize(s);
-	
-	std::cout << "Hello World!!" << std::endl;
 	return 0;
 }
