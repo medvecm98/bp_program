14ead64aa703e0b8582586b94331f3f2b759a143
Michal Medvecky <miskomedvecky@gmail.com>
2021-06-21 17:19:00 +0200

    New factories.

--- Message.cpp
+++ Message.cpp
@@ -1,26 +1,145 @@
 #include "Message.h"
 
-np2ps::Message MessageFactoryWrapper::IpAddressFactory(pk_t from, pk_t to) {
-	np2ps::Message msg;
-	msg.set_from(from);
-	msg.set_to(to);
-	msg.set_msg_type(np2ps::IP_ADDRESS);
-	msg.set_msg_ctx(np2ps::REQUEST);
+using MFW = MessageFactoryWrapper;
+
+void set_from_to(unique_ptr_message& upm, pk_t from, pk_t to) {
+	upm->set_from(from);
+	upm->set_to(to);
+}
+
+unique_ptr_message upm_factory() {
+	return std::make_unique<proto_message>();
+}
+
+unique_ptr_message MFW::IpAddressFactory(pk_t from, pk_t to) {
+	unique_ptr_message msg = std::make_unique<proto_message>();
+	msg->set_from(from);
+	msg->set_to(to);
+	msg->set_msg_type(np2ps::IP_ADDRESS);
+	msg->set_msg_ctx(np2ps::REQUEST);
 	return std::move(msg);
 }
 
-np2ps::Message MessageFactoryWrapper::MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin) {
-	np2ps::Message msg;
-	msg.set_from(from);
-	msg.set_to(to);
-	msg.set_msg_type(np2ps::UPDATE_MARGIN);
-	msg.set_msg_ctx(np2ps::REQUEST);
+unique_ptr_message MFW::MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin) {
+	unique_ptr_message msg = std::make_unique<proto_message>();
+	msg->set_from(from);
+	msg->set_to(to);
+	msg->set_msg_type(np2ps::UPDATE_MARGIN);
+	msg->set_msg_ctx(np2ps::REQUEST);
 
-	msg.mutable_update_margin()->set_m_action(np2ps::ADD);
-	msg.mutable_update_margin()->set_article_pk(article_hash);
+	msg->mutable_update_margin()->set_m_action(np2ps::ADD);
+	msg->mutable_update_margin()->set_article_pk(article_hash);
 
-	msg.mutable_update_margin()->mutable_margin()->set_type(margin.type);
-	msg.mutable_update_margin()->mutable_margin()->set_content(margin.content);
+	msg->mutable_update_margin()->mutable_margin()->set_type(margin.type);
+	msg->mutable_update_margin()->mutable_margin()->set_content(margin.content);
 	return std::move(msg);
 }
 
+unique_ptr_message MFW::ArticleDataChangeFactory(pk_t from, pk_t to, hash_t article_hash, bool download) {
+	unique_ptr_message msg = std::make_unique<proto_message>();
+	msg->set_from(from);
+	msg->set_to(to);
+	msg->set_msg_type(np2ps::ARTICLE_DATA_UPDATE);
+	msg->set_msg_ctx(np2ps::REQUEST);
+	msg->mutable_article_data_update()->set_article_pk(article_hash);
+
+	if (download) {
+		msg->mutable_article_data_update()->set_article_action(np2ps::DOWNLOAD);
+	}
+	else {
+		msg->mutable_article_data_update()->set_article_action(np2ps::REMOVAL);
+	}
+
+	return std::move(msg);
+	//return msg;
+}
+
+unique_ptr_message MFW::ArticleDownloadFactory(pk_t from, pk_t to, hash_t article_hash, level_t level) {
+	unique_ptr_message msg = std::make_unique<proto_message>();
+
+	msg->set_from(from);
+	msg->set_to(to);
+	msg->set_msg_ctx(np2ps::REQUEST);
+	msg->set_msg_type(np2ps::ARTICLE_ALL);
+
+	msg->mutable_article_all()->set_article_hash(article_hash);
+	msg->mutable_article_all()->set_level(level);
+
+	return std::move(msg);
+}
+
+unique_ptr_message MFW::ArticleListFactory(pk_t from, pk_t to, category_container categories) {
+	auto msg = upm_factory();
+	set_from_to(msg, from, to);
+
+	msg->set_msg_ctx(np2ps::REQUEST);
+	msg->set_msg_type(np2ps::ARTICLE_LIST);
+
+	if (!categories.empty()) {
+		msg->mutable_article_list()->set_all_articles(false);
+		for (auto &&cat : categories) {
+			msg->mutable_article_list()->add_categories(cat);
+		}
+	}
+	else {
+		msg->mutable_article_list()->set_all_articles(true);
+	}
+
+	return std::move(msg);
+}
+
+unique_ptr_message MessageFactoryWrapper::ArticleListFactory(pk_t from, pk_t to) {
+	return ArticleListFactory(from, to, category_container());
+}
+
+unique_ptr_message MFW::UserIsMemberFactory(pk_t from, pk_t to, pk_t user_pk, level_t level) {
+	auto msg = upm_factory();
+	set_from_to(msg, from, to);
+
+	msg->set_msg_type(np2ps::USER_IS_MEMBER);
+	msg->set_msg_ctx(np2ps::REQUEST);
+
+	msg->mutable_user_is_member()->set_user_pk(user_pk);
+	msg->mutable_user_is_member()->set_level(level);
+
+	return std::move(msg);
+}
+
+unique_ptr_message update_margin_factory(pk_t from, pk_t to, hash_t article_hash, margin_ptr& margin) {
+	auto msg = upm_factory();
+	set_from_to(msg, from, to);
+
+	msg->set_msg_type(np2ps::UPDATE_MARGIN);
+	msg->set_msg_ctx(np2ps::REQUEST);
+
+	msg->mutable_update_margin()->set_article_pk(article_hash);
+
+	msg->mutable_update_margin()->mutable_margin()->set_content(margin->content);
+	msg->mutable_update_margin()->mutable_margin()->set_type(margin->type);
+
+	return std::move(msg);
+}
+
+unique_ptr_message MFW::UpdateMarginAddFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin) {
+	auto msg = update_margin_factory(from, to, article_hash, margin);
+
+	msg->mutable_update_margin()->set_m_action(np2ps::ADD);
+
+	return std::move(msg);
+}
+
+unique_ptr_message MFW::UpdateMarginRemoveFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin) {
+	auto msg = update_margin_factory(from, to, article_hash, margin);
+
+	msg->mutable_update_margin()->set_m_action(np2ps::REMOVE);
+
+	return std::move(msg);
+}
+
+unique_ptr_message MFW::UpdateMarginUpdateFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin) {
+	auto msg = update_margin_factory(from, to, article_hash, margin);
+
+	msg->mutable_update_margin()->set_m_action(np2ps::UPDATE);
+
+	return std::move(msg);
+}
--- Message.h
+++ Message.h
@@ -4,12 +4,23 @@
 #include "GlobalUsing.h"
 #include "Article.h"
 #include "IpWrapper.h"
+#include "Networking.h"
 #include "protobuf_source/messages.pb.h"
+
+using margin_ptr = std::unique_ptr<Margin>;
 
 class MessageFactoryWrapper {
 public:
-	static np2ps::Message IpAddressFactory(pk_t from, pk_t to);
-	static np2ps::Message MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin);
+	static unique_ptr_message IpAddressFactory(pk_t from, pk_t to);
+	static unique_ptr_message MarginAddFactory(pk_t from, pk_t to, hash_t article_hash, const Margin& margin);
+	static unique_ptr_message ArticleDataChangeFactory(pk_t from, pk_t to, hash_t article_hash, bool download);
+	static unique_ptr_message ArticleDownloadFactory(pk_t from, pk_t to, hash_t article_hash, level_t level);
+	static unique_ptr_message ArticleListFactory(pk_t from, pk_t to, category_container categories);
+	static unique_ptr_message ArticleListFactory(pk_t from, pk_t to);
+	static unique_ptr_message UserIsMemberFactory(pk_t from, pk_t to, pk_t user_pk, level_t level);
+	static unique_ptr_message UpdateMarginAddFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin);
+	static unique_ptr_message UpdateMarginRemoveFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin);
+	static unique_ptr_message UpdateMarginUpdateFactory(pk_t from, pk_t to, hash_t article_hash, margin_ptr margin);
 };
 
 
