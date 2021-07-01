7c3fcfbc22803c88684d527916030eb5566fff68
Michal Medvecky <miskomedvecky@gmail.com>
2021-07-01 11:27:24 +0200

    Added user container usings

--- GlobalUsing.h
+++ GlobalUsing.h
@@ -5,6 +5,8 @@
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
+#include <optional>
+#include <variant>
 
 using pk_t = std::size_t;
 
@@ -12,7 +14,12 @@ using my_string = std::string;
 using hash_t = std::size_t;
 using level_t = std::uint32_t;
 using user_container = std::unordered_set<pk_t>;
+using user_container_citer = user_container::const_iterator;
+using user_level_map = std::unordered_map<pk_t, level_t>;
+using user_level_map_citer = user_level_map::const_iterator;
 
+using optional_my_string = std::optional<my_string>;
 
+using user_variant = std::variant<user_container_citer, user_level_map_citer>;
 
 #endif //PROGRAM_GLOBALUSING_H
