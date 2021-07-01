4b79201f070de5c7a54904eddb4d0b7bc16a5552
Michal Medvecky <miskomedvecky@gmail.com>
2021-07-01 11:27:50 +0200

    Added constructors

--- Margins.h
+++ Margins.h
@@ -1,12 +1,22 @@
-//
-// Created by michal on 27. 3. 2021.
-//
-
 #ifndef PROGRAM_MARGINS_H
 #define PROGRAM_MARGINS_H
 #include "GlobalUsing.h"
+#include <random>
 
 struct Margin {
+	Margin() = default;
+	Margin(const my_string& t, const my_string& c) {
+		type = t;
+		content = c;
+		std::random_device rd("/dev/urandom");
+		id = rd();
+	}
+	Margin(const my_string& t, const my_string& c, unsigned int i) {
+		type = t;
+		content = c;
+		id = i;
+	}
+	unsigned int id;
 	my_string type;
 	my_string content;
 };
